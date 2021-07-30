4b2c83893ddce5d1edab950e6d2ee29ec8eef4d7
Claudinei B Jr <claudinei.brito@monitoratec.com.br>
2021-07-30 15:43:16 -0300

    Cleaning projects and deleting unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +21,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
