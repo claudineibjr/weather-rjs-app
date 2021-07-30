932f901e33dd2738d508248bf42d39daccfac9a2
Claudinei B Jr <claudinei.brito@monitoratec.com.br>
2021-07-30 18:26:06 -0300

    Creation of WeatherHourlyInfo component

---
+++ src/components/WeatherHourlyInfo/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { WeatherInfo } from '../../data/model/WeatherInfo';
+
+type WeatherHourlyInfoProps = {
+  weatherDailyInfo: Array<WeatherInfo>
+}
+
+export const WeatherHourlyInfo = ({ weatherDailyInfo }: WeatherHourlyInfoProps) => (
+    <div/>
+);
