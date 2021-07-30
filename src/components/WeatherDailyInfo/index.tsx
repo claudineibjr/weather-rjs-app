1ac591b87b22f971a25f03baded9d3c34978f172
Claudinei B Jr <claudinei.brito@monitoratec.com.br>
2021-07-30 18:22:39 -0300

    Creation of WeatherDailyInfo component

---
+++ src/components/WeatherDailyInfo/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { WeatherInfo } from '../../data/model/WeatherInfo';
+
+type WeatherDailyInfoProps = {
+  weatherDailyInfo: WeatherInfo
+}
+
+export const WeatherDailyInfo = ({ weatherDailyInfo }: WeatherDailyInfoProps) => (
+    <div/>
+);
