ef082ca3a1a25db9ebcd7faca4cae418a11ccc5b
Claudinei B Jr <claudinei.brito@monitoratec.com.br>
2021-08-02 10:52:26 -0300

    Creation of HomePage component

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { HomePage } from './pages/HomePage';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HomePage />
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { WeatherDailyInfo } from '../../components/WeatherDailyInfo';
+import { OpenWeatherMapWeekResponse } from '../../data/model/OpenWeatherResponse/OpenWeatherMapWeekResponse';
+import { WeatherInfoMap } from '../../data/model/WeatherInfo/response/WeatherInfoMap';
+import { loadWeekWeatherInfo } from '../../services/OpenWeatherMapApi';
+import './index.css';
+
+interface IProps {
+
+}
+
+interface IState {
+    weekWeatherInfo?: OpenWeatherMapWeekResponse
+}
+
+export class HomePage extends Component<IProps, IState> {
+
+    constructor(props: IProps){
+        super(props);
+
+        this.state = {
+            weekWeatherInfo: undefined
+        };
+    }
+    
+    componentDidMount = () => {
+        const weekWeatherInfo = loadWeekWeatherInfo();
+        this.setState({weekWeatherInfo: weekWeatherInfo});
+    }
+
+    render() {
+        const {weekWeatherInfo} = this.state;
+        
+        return (
+            <div className="Main">
+                {weekWeatherInfo != undefined &&
+                    weekWeatherInfo.daily.map((dayWeatherInfo, key) => 
+                        <WeatherDailyInfo weatherDailyInfo={WeatherInfoMap.toWeatherInfo(dayWeatherInfo)} />
+                    )
+                }
+            </div>
+        );
+    }
+
+}
