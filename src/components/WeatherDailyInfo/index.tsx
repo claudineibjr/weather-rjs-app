7ae267ba20a2825ae38018c2e7f2e3fa90241bc7
Claudinei B Jr <claudinei.brito@monitoratec.com.br>
2021-08-02 11:40:29 -0300

    Adjusts at components styles

--- src/components/WeatherDailyInfo/index.tsx
+++ src/components/WeatherDailyInfo/index.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import { WeatherInfo } from '../../data/model/WeatherInfo/WeatherInfo';
 import { DateUtilities } from '../../utils/utils';
+import './styles.css';
 
 type WeatherDailyInfoProps = {
   weatherDailyInfo: WeatherInfo
 }
 
 export const WeatherDailyInfo = ({ weatherDailyInfo }: WeatherDailyInfoProps) => (
-    <div>
-      {DateUtilities.shortDays[weatherDailyInfo.date.getDay()]} <br/>
+    <div className="WeatherDailyInfoMain">
+        <div className="DayName">
+          {DateUtilities.shortDays[weatherDailyInfo.date.getDay()]} <br/>
+        </div>
       
-      <img src={weatherDailyInfo.wheaterInfoIcon} />
+      <img className="WeatherIcon"
+        src={weatherDailyInfo.wheaterInfoIcon} />
 
-      <div>
-        {weatherDailyInfo.maxTemperature | 0}º
-        {weatherDailyInfo.minTemperature | 0}º
+      <div className="Temperatures">
+        <div className="MaxTemperature">
+          {weatherDailyInfo.maxTemperature | 0}º
+        </div>
+        <div className="MinTemperature">
+          {weatherDailyInfo.minTemperature | 0}º
+        </div>
       </div>
     </div>
 );
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { WeatherDailyInfo } from '../../components/WeatherDailyInfo';
 import { OpenWeatherMapWeekResponse } from '../../data/model/OpenWeatherResponse/OpenWeatherMapWeekResponse';
 import { WeatherInfoMap } from '../../data/model/WeatherInfo/response/WeatherInfoMap';
+import { WeatherInfo } from '../../data/model/WeatherInfo/WeatherInfo';
 import { loadWeekWeatherInfo } from '../../services/OpenWeatherMapApi';
 import './styles.css';
 
@@ -10,7 +11,7 @@ interface IProps {
 }
 
 interface IState {
-    weekWeatherInfo?: OpenWeatherMapWeekResponse
+    weekWeatherInfos: Array<WeatherInfo>
 }
 
 export class HomePage extends Component<IProps, IState> {
@@ -19,25 +20,32 @@ export class HomePage extends Component<IProps, IState> {
         super(props);
 
         this.state = {
-            weekWeatherInfo: undefined
+            weekWeatherInfos: []
         };
     }
     
     componentDidMount = () => {
         const weekWeatherInfo = loadWeekWeatherInfo();
-        this.setState({weekWeatherInfo: weekWeatherInfo});
+        if (weekWeatherInfo !== undefined) {
+            const weekWeatherInfos = weekWeatherInfo.daily.map((weatherInfoMap, _) => WeatherInfoMap.toWeatherInfo(weatherInfoMap));
+            // weekWeatherInfos.filter((weekWeatherInfo, _, __) => weekWeatherInfo.)
+            
+            this.setState({weekWeatherInfos: weekWeatherInfos});
+        }
     }
 
     render() {
-        const {weekWeatherInfo} = this.state;
+        const {weekWeatherInfos} = this.state;
         
         return (
-            <div className="Main">
-                {weekWeatherInfo != undefined &&
-                    weekWeatherInfo.daily.map((dayWeatherInfo, key) => 
-                        <WeatherDailyInfo weatherDailyInfo={WeatherInfoMap.toWeatherInfo(dayWeatherInfo)} />
-                    )
-                }
+            <div className="HomePageMain">
+                <div className="WeekWeatherInfo">
+                    {weekWeatherInfos != undefined &&
+                        weekWeatherInfos.map((dayWeatherInfo, key) => 
+                            <WeatherDailyInfo weatherDailyInfo={dayWeatherInfo} />
+                        )
+                    }
+                </div>
             </div>
         );
     }
