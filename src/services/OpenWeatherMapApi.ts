f1ed7dc29e708ec42e89893a17665bed25634df8
Claudinei B Jr <claudinei.brito@monitoratec.com.br>
2021-08-03 20:54:13 -0300

    TODO: Keep mocked data to avoid unnecessary calls to API

--- src/services/OpenWeatherMapApi.ts
+++ src/services/OpenWeatherMapApi.ts
@@ -1,24 +1,36 @@
 import axios from "axios";
 import { plainToClass } from "class-transformer";
+import loadWeekWeatherInfoMockedData from "../data/mock/loadWeekWeatherInfo.json";
 import loadDayWeatherInfoMockedData from "../data/mock/loadDayWeatherInfo.json";
 import { OpenWeatherMapDayResponse } from "../data/model/OpenWeatherResponse/OpenWeatherMapDayResponse";
 import { OpenWeatherMapWeekResponse } from "../data/model/OpenWeatherResponse/OpenWeatherMapWeekResponse";
 
-export async function loadWeekWeatherInfo(): Promise<OpenWeatherMapWeekResponse | undefined> {
-    try {
-        const response = await axios.get(
-            'https://api.openweathermap.org/data/2.5/onecall?lat=-21.9956977&lon=-47.9515105&exclude=minutely,hourly,alerts,current&appid=5bf4009a1c9ac711a96acf649074854f&units=imperial'
-        );
+const sleep = (milliseconds: number) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
 
-        if (response.status === 200) {
-            let openWeatherMapWeekResponse: OpenWeatherMapWeekResponse = plainToClass(OpenWeatherMapWeekResponse, response.data as Object);
-            return openWeatherMapWeekResponse;
-        } else {
-            return undefined;
-        }
-    } catch (_) {
-        return undefined;
-    }
+export async function loadWeekWeatherInfo(): Promise<OpenWeatherMapWeekResponse | undefined> {
+    // TODO: Map the data from JSON to OpenWeatherMapWeekResponse
+    let openWeatherMapWeekResponse: OpenWeatherMapWeekResponse = plainToClass(OpenWeatherMapWeekResponse, loadWeekWeatherInfoMockedData as Object);
+
+    await sleep(1000);
+
+    return openWeatherMapWeekResponse;
+
+    // try {
+    //     const response = await axios.get(
+    //         'https://api.openweathermap.org/data/2.5/onecall?lat=-21.9956977&lon=-47.9515105&exclude=minutely,hourly,alerts,current&appid=5bf4009a1c9ac711a96acf649074854f&units=imperial'
+    //     );
+
+    //     if (response.status === 200) {
+    //         let openWeatherMapWeekResponse: OpenWeatherMapWeekResponse = plainToClass(OpenWeatherMapWeekResponse, response.data as Object);
+    //         return openWeatherMapWeekResponse;
+    //     } else {
+    //         return undefined;
+    //     }
+    // } catch (_) {
+    //     return undefined;
+    // }
 }
 
 export function loadDayWeatherInfo(): OpenWeatherMapDayResponse | undefined {
