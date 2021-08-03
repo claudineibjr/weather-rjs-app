253e63a8ca210c5c6d558b3d32a512c0c10f60f7
Claudinei B Jr <claudinei.brito@monitoratec.com.br>
2021-08-03 20:51:17 -0300

    Changing from mock from real data at loadWeekWeatherInfo

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -3,26 +3,32 @@ import { WeatherDailyInfo } from '../../components/WeatherDailyInfo';
 import { WeatherInfoMap } from '../../data/model/WeatherInfo/response/WeatherInfoMap';
 import { DailyWeatherInfo } from '../../data/model/WeatherInfo/DailyWeatherInfo';
 import { loadWeekWeatherInfo } from '../../services/OpenWeatherMapApi';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import './styles.scss';
 import DefaultAppBar from '../../components/DefaultAppBar';
 
 export function HomePage() {
     const [weekWeatherInfos, setWeekWeatherInfos] = useState<Array<DailyWeatherInfo>>([]);
+    const [isLoading, setLoading] = useState<boolean>(false);
 
     useEffect(() => {
-        const weekWeatherInfo = loadWeekWeatherInfo();
-        if (weekWeatherInfo !== undefined) {
-            let weekWeatherInfos = weekWeatherInfo.daily.map((weatherInfoMap, _) => WeatherInfoMap.toDailyWeatherInfo(weatherInfoMap));
-            weekWeatherInfos.sort((weatherInfoA, weatherInfoB) => weatherInfoB.date.getTime() - weatherInfoA.date.getTime());
-            if (weekWeatherInfos.length > 5) {
-                weekWeatherInfos = weekWeatherInfos.slice(0, 5);
-            }
+        async function fetchMyAPI() {
+            setLoading(true);
 
-            setWeekWeatherInfos(weekWeatherInfos);
+            const weekWeatherInfo = await loadWeekWeatherInfo();
+            if (weekWeatherInfo !== undefined) {
+                let weekWeatherInfos = weekWeatherInfo.daily.map((weatherInfoMap, _) => WeatherInfoMap.toDailyWeatherInfo(weatherInfoMap));
+                weekWeatherInfos.sort((weatherInfoA, weatherInfoB) => weatherInfoA.date.getTime() - weatherInfoB.date.getTime());
+                if (weekWeatherInfos.length > 5) {
+                    weekWeatherInfos = weekWeatherInfos.slice(0, 5);
+                }
+
+                setWeekWeatherInfos(weekWeatherInfos);
+            }
+            setLoading(false);
         }
+
+        fetchMyAPI();
     }, []);
 
     return (
@@ -30,7 +36,9 @@ export function HomePage() {
             <DefaultAppBar />
 
             <div className="WeekWeatherInfo">
-                {weekWeatherInfos !== undefined &&
+                {isLoading ?
+                    <CircularProgress />
+                    : weekWeatherInfos !== undefined &&
                     weekWeatherInfos.map((dayWeatherInfo, _) =>
                         <WeatherDailyInfo weatherDailyInfo={dayWeatherInfo} />
                     )
--- src/services/OpenWeatherMapApi.ts
+++ src/services/OpenWeatherMapApi.ts
@@ -1,14 +1,24 @@
+import axios from "axios";
 import { plainToClass } from "class-transformer";
-import loadWeekWeatherInfoMockedData from "../data/mock/loadWeekWeatherInfo.json";
 import loadDayWeatherInfoMockedData from "../data/mock/loadDayWeatherInfo.json";
 import { OpenWeatherMapDayResponse } from "../data/model/OpenWeatherResponse/OpenWeatherMapDayResponse";
 import { OpenWeatherMapWeekResponse } from "../data/model/OpenWeatherResponse/OpenWeatherMapWeekResponse";
 
-export function loadWeekWeatherInfo(): OpenWeatherMapWeekResponse | undefined {
-    // TODO: Map the data from JSON to OpenWeatherMapWeekResponse
-    let openWeatherMapWeekResponse: OpenWeatherMapWeekResponse = plainToClass(OpenWeatherMapWeekResponse, loadWeekWeatherInfoMockedData as Object);
+export async function loadWeekWeatherInfo(): Promise<OpenWeatherMapWeekResponse | undefined> {
+    try {
+        const response = await axios.get(
+            'https://api.openweathermap.org/data/2.5/onecall?lat=-21.9956977&lon=-47.9515105&exclude=minutely,hourly,alerts,current&appid=5bf4009a1c9ac711a96acf649074854f&units=imperial'
+        );
 
-    return openWeatherMapWeekResponse;
+        if (response.status === 200) {
+            let openWeatherMapWeekResponse: OpenWeatherMapWeekResponse = plainToClass(OpenWeatherMapWeekResponse, response.data as Object);
+            return openWeatherMapWeekResponse;
+        } else {
+            return undefined;
+        }
+    } catch (_) {
+        return undefined;
+    }
 }
 
 export function loadDayWeatherInfo(): OpenWeatherMapDayResponse | undefined {
