d27f373b72390350487f0321392adcfff147e468
Claudinei B Jr <claudinei.brito@monitoratec.com.br>
2021-08-03 12:11:06 -0300

    Transformin HourlyWeatherInfoPage into Function

--- src/pages/HourlyWeatherInfo/index.tsx
+++ src/pages/HourlyWeatherInfo/index.tsx
@@ -1,28 +1,14 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import { WeatherHourlyChart } from "../../components/WeatherHourlyChart";
 import { HourlyWeatherInfo } from "../../data/model/WeatherInfo/HourlyWeatherInfo";
 import { WeatherInfoMap } from "../../data/model/WeatherInfo/response/WeatherInfoMap";
 import { loadDayWeatherInfo } from "../../services/OpenWeatherMapApi";
 import './styles.scss';
 
-interface IProps {
+export default function HourlyWeatherInfoPage() {
+    const [dayWeatherInfos, setDayWeatherInfos] = useState<Array<HourlyWeatherInfo> | undefined>(undefined);
 
-}
-
-interface IState {
-    dayWeatherInfos?: Array<HourlyWeatherInfo>
-}
-
-export default class HourlyWeatherInfoPage extends Component<IProps, IState> {
-    constructor(props: IProps) {
-        super(props);
-
-        this.state = {
-            dayWeatherInfos: undefined,
-        };
-    }
-
-    componentDidMount = () => {
+    useEffect(() => {
         const dayWeatherInfo = loadDayWeatherInfo();
         if (dayWeatherInfo !== undefined) {
             let dayWeatherInfos: Array<HourlyWeatherInfo> = dayWeatherInfo.hourly.map((weatherInfoMap, _) => WeatherInfoMap.toHourlyWeatherInfo(weatherInfoMap));
@@ -31,23 +17,19 @@ export default class HourlyWeatherInfoPage extends Component<IProps, IState> {
             let indexOfFirstOfDay = dayWeatherInfos.indexOf(firstOfDay);
             dayWeatherInfos = dayWeatherInfos.slice(indexOfFirstOfDay, indexOfFirstOfDay + 24);
 
-            this.setState({ dayWeatherInfos: dayWeatherInfos });
+            setDayWeatherInfos(dayWeatherInfos);
         }
-    }
+    }, []);
 
-    render() {
-        const { dayWeatherInfos } = this.state;
-        
-        return (
-            <div className="HourlyWeatherInfoMain">
-                {dayWeatherInfos !== undefined ?
-                    <div className="HourlyWeatherInfoChart">
-                        <WeatherHourlyChart
-                            weatherDailyInfo={dayWeatherInfos}/>
-                    </div>
-                    : 'Loading...'
-                }
-            </div>
-        );
-    };
+    return (
+        <div className="HourlyWeatherInfoMain">
+            {dayWeatherInfos !== undefined ?
+                <div className="HourlyWeatherInfoChart">
+                    <WeatherHourlyChart
+                        weatherDailyInfo={dayWeatherInfos} />
+                </div>
+                : 'Loading...'
+            }
+        </div>
+    );
 }
