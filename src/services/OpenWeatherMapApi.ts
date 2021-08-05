e49d6d67620cfab4c6b5704827005ea425dff24e
Claudinei B Jr <claudinei.brito@monitoratec.com.br>
2021-08-05 16:38:05 -0300

    Removing mock call

--- src/services/OpenWeatherMapApi.ts
+++ src/services/OpenWeatherMapApi.ts
@@ -10,59 +10,59 @@ export const sleep = (milliseconds: number) => {
 }
 
 export async function loadWeekWeatherInfo(latitude: number, longitude: number): Promise<OpenWeatherMapWeekResponse | undefined> {
-    try {
-        // TODO: Map the data from JSON to OpenWeatherMapWeekResponse
-        let openWeatherMapWeekResponse: OpenWeatherMapWeekResponse = plainToClass(OpenWeatherMapWeekResponse, loadWeekWeatherInfoMockedData as Object);
-
-        await sleep(250);
-
-        return openWeatherMapWeekResponse;
-    } catch (_) {
-        return undefined;
-    }
-
     // try {
-    //     const OPEN_WHEATER_MAP_API_KEY = process.env.REACT_APP_OPEN_WHEATER_MAP_API_KEY;
-    //     const response = await axios.get(
-    //         `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,alerts,current&appid=${OPEN_WHEATER_MAP_API_KEY}&units=imperial`
-    //     );
+    //     // TODO: Map the data from JSON to OpenWeatherMapWeekResponse
+    //     let openWeatherMapWeekResponse: OpenWeatherMapWeekResponse = plainToClass(OpenWeatherMapWeekResponse, loadWeekWeatherInfoMockedData as Object);
 
-    //     if (response.status === 200) {
-    //         let openWeatherMapWeekResponse: OpenWeatherMapWeekResponse = plainToClass(OpenWeatherMapWeekResponse, response.data as Object);
-    //         return openWeatherMapWeekResponse;
-    //     } else {
-    //         return undefined;
-    //     }
+    //     await sleep(250);
+
+    //     return openWeatherMapWeekResponse;
     // } catch (_) {
     //     return undefined;
     // }
+
+    try {
+        const OPEN_WHEATER_MAP_API_KEY = process.env.REACT_APP_OPEN_WHEATER_MAP_API_KEY;
+        const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,alerts,current&appid=${OPEN_WHEATER_MAP_API_KEY}&units=imperial`
+        );
+
+        if (response.status === 200) {
+            let openWeatherMapWeekResponse: OpenWeatherMapWeekResponse = plainToClass(OpenWeatherMapWeekResponse, response.data as Object);
+            return openWeatherMapWeekResponse;
+        } else {
+            return undefined;
+        }
+    } catch (_) {
+        return undefined;
+    }
 }
 
 export async function load5Days3HoursForecastWeatherInfo(latitude: number, longitude: number): Promise<OpenWeatherMap5Day3HoursWeatherInfoResponse | undefined> {
-    try {
-        // TODO: Map the data from JSON to OpenWeatherMapWeekResponse
-        let openWeatherMap5Day3HoursWeatherInfoResponse: OpenWeatherMap5Day3HoursWeatherInfoResponse = plainToClass(OpenWeatherMap5Day3HoursWeatherInfoResponse, load5Day3HoursWeatherForecastInfoMockedData as Object);
-
-        await sleep(250);
-
-        return openWeatherMap5Day3HoursWeatherInfoResponse;
-    } catch (_) {
-        return undefined;
-    }
-
     // try {
-    //     const OPEN_WHEATER_MAP_API_KEY = process.env.REACT_APP_OPEN_WHEATER_MAP_API_KEY;
-    //     const response = await axios.get(
-    //         `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${OPEN_WHEATER_MAP_API_KEY}&units=imperial`
-    //     );
+    //     // TODO: Map the data from JSON to OpenWeatherMapWeekResponse
+    //     let openWeatherMap5Day3HoursWeatherInfoResponse: OpenWeatherMap5Day3HoursWeatherInfoResponse = plainToClass(OpenWeatherMap5Day3HoursWeatherInfoResponse, load5Day3HoursWeatherForecastInfoMockedData as Object);
 
-    //     if (response.status === 200) {
-    //         const openWeatherMap5Day3HoursWeatherInfoResponse: OpenWeatherMap5Day3HoursWeatherInfoResponse = plainToClass(OpenWeatherMap5Day3HoursWeatherInfoResponse, response.data as Object);
-    //         return openWeatherMap5Day3HoursWeatherInfoResponse;
-    //     } else {
-    //         return undefined;
-    //     }
+    //     await sleep(250);
+
+    //     return openWeatherMap5Day3HoursWeatherInfoResponse;
     // } catch (_) {
     //     return undefined;
     // }
+
+    try {
+        const OPEN_WHEATER_MAP_API_KEY = process.env.REACT_APP_OPEN_WHEATER_MAP_API_KEY;
+        const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${OPEN_WHEATER_MAP_API_KEY}&units=imperial`
+        );
+
+        if (response.status === 200) {
+            const openWeatherMap5Day3HoursWeatherInfoResponse: OpenWeatherMap5Day3HoursWeatherInfoResponse = plainToClass(OpenWeatherMap5Day3HoursWeatherInfoResponse, response.data as Object);
+            return openWeatherMap5Day3HoursWeatherInfoResponse;
+        } else {
+            return undefined;
+        }
+    } catch (_) {
+        return undefined;
+    }
 }
