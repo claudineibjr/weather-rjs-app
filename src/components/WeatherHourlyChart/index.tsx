2c3c30499aebf8d2f197fc283aee6bc501bf8bf5
Claudinei B Jr <claudinei.brito@monitoratec.com.br>
2021-08-05 12:49:46 -0300

    Improvement at WeatherHourlyChart

--- src/components/WeatherHourlyChart/index.tsx
+++ src/components/WeatherHourlyChart/index.tsx
@@ -7,12 +7,15 @@ import { Group } from '@vx/group';
 import { AreaClosed } from '@vx/shape';
 import { LinearGradient } from '@vx/gradient';
 import { extent, max, min } from 'd3-array';
+import useWindowDimensions from '../../hook/useWindowDimensionsHook';
 
 type WeatherHourlyInfoProps = {
   weatherDailyInfo: Array<HourlyWeatherInfo>
 }
 
 export const WeatherHourlyChart = ({ weatherDailyInfo }: WeatherHourlyInfoProps) => {
+  const { width } = useWindowDimensions();
+  
   // Dimensions
   const margin = {
     top: 30,
@@ -21,12 +24,12 @@ export const WeatherHourlyChart = ({ weatherDailyInfo }: WeatherHourlyInfoProps)
     bottom: 80,
   };
 
-  const width = 640 + margin.left + margin.right;
-  const height = 360 + margin.top + margin.bottom;
+  const chartWidth = width * 0.8;
+  const chartHeight = 360 + margin.top + margin.bottom;
 
   // Bounds
-  const innerWidth = width - margin.left - margin.right;
-  const innerHeight = height - margin.top - margin.bottom;
+  const innerWidth = chartWidth - margin.left - margin.right;
+  const innerHeight = chartHeight - margin.top - margin.bottom;
 
   // Accessors
   const getDate = (d: HourlyWeatherInfo): Date => d.date;
@@ -56,7 +59,7 @@ export const WeatherHourlyChart = ({ weatherDailyInfo }: WeatherHourlyInfoProps)
 
   return (
     <div className="WeatherHourlyChartMain">
-      <svg width={width} height={height}>
+      <svg width={chartWidth} height={chartHeight}>
         <rect
           x={margin.left}
           y={margin.top}
