05d8c3183e92f064ed23ae3c5a43876782c0c19b
Claudinei B Jr <claudinei.brito@monitoratec.com.br>
2021-08-05 12:23:21 -0300

    Adjust at UserLocation

--- src/data/model/UserPreferences/UserLocation.ts
+++ src/data/model/UserPreferences/UserLocation.ts
@@ -1,10 +1,7 @@
-// import { OpenWeatherMapGeocodingResponse } from "../OpenWeatherResponse/OpenWeatherMapGeocodingResponse";
-
 export default class UserLocation {
     latitude: number;
     longitude: number;
     name?: string;
-    country?: string;
 
     constructor(latitude: number, longitude: number) {
         this.latitude = latitude;
@@ -17,23 +14,7 @@ export default class UserLocation {
             -2.2952338
         );
         oldTraffordStadiumLocation.name = "Trafford Park";
-        oldTraffordStadiumLocation.country = "GB";
 
         return oldTraffordStadiumLocation;
     }
-
-    // static userLocationFromResponse(openWeatherMapGeocodingResponse?: OpenWeatherMapGeocodingResponse): UserLocation {
-    //     if (openWeatherMapGeocodingResponse === undefined) {
-    //         throw new Error('Error on parsing');
-    //     }
-
-    //     const userLocation: UserLocation = new UserLocation(
-    //         openWeatherMapGeocodingResponse.lat,
-    //         openWeatherMapGeocodingResponse.lon
-    //     );
-    //     userLocation.name = openWeatherMapGeocodingResponse.name;
-    //     userLocation.country = openWeatherMapGeocodingResponse.country;
-
-    //     return userLocation;
-    // }
 }
